019b3e62ba18881b65e8251cca55511a73d443a8
Tanmay2510 <comparisionthing@gmail.com>
2023-02-23 01:00:35 +0530

    left register

--- frontend/fe/src/component/Form/Reglog.jsx
+++ frontend/fe/src/component/Form/Reglog.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import "./Reglog.css"
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 function Reglog() {
   const {isRegLog,setRegLog} = useState(false);
   const [ user, setUser] = useState({
@@ -15,13 +18,36 @@ const handleChange = e => {
 }
   return (
     <div className='ReglogCont'>
-    <form 
-            // onSubmit={login}
-            >
-            <input type="email" name="email" value={user.email} onChange={handleChange} placeholder="Enter your Email"></input>
-            <input type="password" name="password" value={user.password} onChange={handleChange}  placeholder="Enter your Password" ></input>
-            <button >Login</button>
-            </form>
+    <h1>User Login</h1>
+    <Form classname='Reglog'>
+    <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Label>Email address</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" />
+      <Form.Text className="text-muted">
+        We'll never share your email with anyone else.
+      </Form.Text>
+    </Form.Group>
+
+    <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+    <div className="d-grid gap-2">
+
+    <button 
+    className='but'
+    type="submit" >
+      Submit
+    </button>
+    <h4 style={{textAlign:"center",margin:"1px"}}>OR</h4>
+    <button 
+    className='but'
+    type="submit" >
+    Register
+  </button>
+    </div>
+
+  </Form>
     </div>
   )
 }
--- frontend/fe/src/component/nav/Nav.jsx
+++ frontend/fe/src/component/nav/Nav.jsx
@@ -3,12 +3,11 @@ import "./Nav.css"
 import logo from "../../assets/logo.png"
 function Nav() {
   return (
-    <nav className='nav'>
+    <nav className='navv'>
         <h1>GoGo</h1>
         <img src={logo} style={{width:"5%"}}></img>
         <div className='innerNav'>
-        <button>TExt HEre 1</button>
-        <button>TExt HEre 2</button>
+        <button >TExt HEre 1</button>
         </div>
 
     </nav>
