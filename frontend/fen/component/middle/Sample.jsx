f28230da387b56a6a898b85947c7d1c622ef2a0b
Tanmay2510 <comparisionthing@gmail.com>
2023-03-15 23:44:41 +0530

    Minor updates

--- frontend/fen/component/middle/Sample.jsx
+++ frontend/fen/component/middle/Sample.jsx
@@ -1,19 +1,32 @@
 import { data } from '@/util/SData'
-import React from 'react'
-
-
+import React, { useState } from 'react'
+import {BsPauseFill} from 'react-icons/bs'
 function Sample() {
+  const [isplay,setisplay] = useState(false);
  
   return (
     <div className='middleSection'>
     {
       data.map((el,i)=>{
         return (
-          <div onClick={()=>{
+          <div
+          className='middleSample'
+          onClick={()=>{
+            setisplay(!isplay)
             var audio = new Audio(el.uri);
+            if(isplay){
             audio.play();
+            }
           }}>
             {el.logo}
+            <div>
+            {
+              isplay ? 
+              <BsPauseFill/>
+              :null
+            }
+            </div>
+
           </div>
         )
       })
