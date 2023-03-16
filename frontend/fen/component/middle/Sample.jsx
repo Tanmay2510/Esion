7a6e950a94533fae400f9f1319d0748ddac41f68
Tanmay2510 <comparisionthing@gmail.com>
2023-03-16 23:26:15 +0530

    Pause left

--- frontend/fen/component/middle/Sample.jsx
+++ frontend/fen/component/middle/Sample.jsx
@@ -2,8 +2,28 @@ import { data } from '@/util/SData'
 import React, { useState } from 'react'
 import {BsPauseFill} from 'react-icons/bs'
 function Sample() {
-  const [isplay,setisplay] = useState(false);
- 
+  // const [isplay,setisplay] = useState(false);
+  const [clickedIndex, setClickedIndex] = useState({});
+  const [isAudio,setIsAudio] = useState(null);
+  var audio;
+  const hanldeSound = (i) => ( )=>{
+    // setisplay(!isplay)
+    setClickedIndex(state => ({
+      ...state, 
+      [i]: !state[i] 
+    }));
+   audio = new Audio(data[i].uri);
+    if(clickedIndex){
+    audio.play();
+    }
+  }
+  const handlePause = (i) => () =>{
+    setIsAudio(i)
+  
+    // console.log(clickedIndex)
+    // audio = new Audio(data[i].uri);
+    // audio.pause();
+  }
   return (
     <div className='middleSection'>
     {
@@ -11,22 +31,21 @@ function Sample() {
         return (
           <div
           className='middleSample'
-          onClick={()=>{
-            setisplay(!isplay)
-            var audio = new Audio(el.uri);
-            if(isplay){
-            audio.play();
-            }
-          }}>
+          onClick={hanldeSound(i)}>
             {el.logo}
-            <div>
             {
-              isplay ? 
-              <BsPauseFill/>
-              :null
-            }
-            </div>
+              clickedIndex[i] ? 
+              <div>
+              <BsPauseFill onClick={()=>{
+                let aud = new Audio(el.uri);
+                aud.pause();
+              {/*handlePause(i)*/}
 
+              }}/>
+              <input type="range"></input>
+              </div>
+              : null
+            }
           </div>
         )
       })
